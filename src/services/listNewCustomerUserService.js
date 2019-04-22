b11e41d17d02761a96211e69131bf8fc391b26a8
Phathdt <Phathdt.leviateam@gmail.com>
2019-04-23 02:17:14 +0700

    convert to async arrow function

--- src/services/listNewCustomerUserService.js
+++ src/services/listNewCustomerUserService.js
@@ -2,15 +2,15 @@ import http from "./httpService";
 
 const apiUrl = "https://5cbc10cafa84180014bdb218.mockapi.io";
 
-export async function getListNewCustomer() {
+const getListNewCustomer = async () => {
   return await http.get(`${apiUrl}/current_customers`);
-}
+};
 
-export async function callCustomer(id) {
+const callCustomer = async id => {
   return await http.post(`${apiUrl}/call`, {
     user_id: id
   });
-}
+};
 
 export default {
   getListNewCustomer,
