d0ab578f5c748a98081bd2b12120ea926a0aada9
Phathdt <Phathdt.leviateam@gmail.com>
2019-04-23 01:43:06 +0700

    update https v2

--- src/components/DashBoard/VideoComponent.js
+++ src/components/DashBoard/VideoComponent.js
@@ -6,7 +6,7 @@ export default class VideoComponent extends Component {
       <div>
         <img
           className="video-screen"
-          src="http://placehold.it/700x400"
+          src="https://placehold.it/700x400"
           alt=""
         />
       </div>
