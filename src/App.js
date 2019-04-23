ec17beccadbf5f2a3f188e0b74f45b6d2522b578
Phathdt <Phathdt.leviateam@gmail.com>
2019-04-24 01:39:14 +0700

    update sidebar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,135 @@
 import React, { Component } from "react";
-import NavBar from "./components/navBar";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Sidebar from "react-sidebar";
+import MaterialTitlePanel from "./components/SideBar/material_title_panel";
+import SidebarContent from "./components/SideBar/sidebar_content";
+
 import DashBoardPage from "./pages/DashBoardPage";
 import CashierPage from "./pages/CashierPage";
 import ListCustomerPage from "./pages/ListCustomerPage";
 import DetailCustomerPage from "./pages/DetailCustomerPage";
 import NotFound from "./pages/NotFound";
 
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+const styles = {
+  contentHeaderMenuLink: {
+    textDecoration: "none",
+    color: "white",
+    padding: 8
+  },
+  sidebar: {
+    zIndex: 3,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    transition: "transform .3s ease-out",
+    WebkitTransition: "-webkit-transform .3s ease-out",
+    willChange: "transform",
+    overflowY: "hidden"
+  },
+  content: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    overflowX: "hidden",
+    WebkitOverflowScrolling: "touch",
+    transition: "left .3s ease-out, right .3s ease-out"
+  }
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      docked: mql.matches,
+      open: false
+    };
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.onSetOpen = this.onSetOpen.bind(this);
+  }
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetOpen(open) {
+    this.setState({ open });
+  }
+
+  mediaQueryChanged() {
+    this.setState({
+      docked: mql.matches,
+      open: false
+    });
+  }
+
+  toggleOpen(ev) {
+    this.setState({ open: !this.state.open });
+
+    if (ev) {
+      ev.preventDefault();
+    }
+  }
+
   render() {
+    const sidebar = <SidebarContent />;
+
+    const toggleMenu = (
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a onClick={this.toggleOpen} style={styles.contentHeaderMenuLink}>
+        =
+      </a>
+    );
+    const contentHeader = (
+      <span>
+        {!this.state.docked && toggleMenu}
+
+        <a href="/" style={styles.contentHeaderMenuLink}>
+          <span> Ever Face </span>
+        </a>
+      </span>
+    );
+
+    const sidebarProps = {
+      sidebar,
+      docked: this.state.docked,
+      open: this.state.open,
+      onSetOpen: this.onSetOpen,
+      pullRight: true
+    };
+
     return (
       <div className="App container h-100">
         <ToastContainer />
-        <NavBar />
         <Router>
-          <Switch>
-            <Route exact path="/" component={DashBoardPage} />
-            <Route path="/cashier" component={CashierPage} />
-            <Route path="/customers" component={ListCustomerPage} />
-            <Route path="/detail_customers" component={DetailCustomerPage} />
-            <Route path="*" component={NotFound} />
-          </Switch>
+          <Sidebar
+            {...sidebarProps}
+            styles={{ sidebar: styles.sidebar, content: styles.content }}
+          >
+            <MaterialTitlePanel title={contentHeader}>
+              <Switch>
+                <Route exact path="/" component={DashBoardPage} />
+                <Route path="/cashier" component={CashierPage} />
+                <Route path="/customers" component={ListCustomerPage} />
+                <Route
+                  path="/detail_customers"
+                  component={DetailCustomerPage}
+                />
+                <Route path="*" component={NotFound} />
+              </Switch>
+            </MaterialTitlePanel>
+          </Sidebar>
         </Router>
       </div>
     );
---
+++ src/components/SideBar/sidebar_content.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MaterialTitlePanel from "./material_title_panel";
+import { Link } from "react-router-dom";
+
+const styles = {
+  sidebar: {
+    width: 256,
+    height: "100%"
+  },
+  sidebarLink: {
+    display: "block",
+    padding: "16px 0px",
+    color: "#757575",
+    textDecoration: "none"
+  },
+  divider: {
+    margin: "8px 0",
+    height: 1,
+    backgroundColor: "#757575"
+  },
+  content: {
+    padding: "16px",
+    height: "100%",
+    backgroundColor: "white"
+  }
+};
+
+const SidebarContent = props => {
+  const style = props.style
+    ? { ...styles.sidebar, ...props.style }
+    : styles.sidebar;
+
+  return (
+    <MaterialTitlePanel title="Menu" style={style}>
+      <div style={styles.content}>
+        <Link to="/" style={styles.sidebarLink}>
+          Lễ Tân
+        </Link>
+        <Link to="/cashier" style={styles.sidebarLink}>
+          Thu Ngân
+        </Link>
+        <Link to="/customers" style={styles.sidebarLink}>
+          Quản lý Khách hàng
+        </Link>
+        <Link to="/detail_customers" style={styles.sidebarLink}>
+          Chi tiết Khách hàng
+        </Link>
+      </div>
+    </MaterialTitlePanel>
+  );
+};
+
+SidebarContent.propTypes = {
+  style: PropTypes.object
+};
+
+export default SidebarContent;
